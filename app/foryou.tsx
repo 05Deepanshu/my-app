ce9735f1972e5290a7fb9718a98e8cdda928219e
deepanshukatariya450@gmail.com <94599793+05Deepanshu@users.noreply.github.com>
2025-05-30 17:02:03 +0530

    Tabs in navigation

---
+++ app/foryou.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from 'react-native';
+
+export function ForYou() {
+    return <View> 
+        <Text>For You Page</Text>
+    </View>;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,23 @@
-import { View } from 'react-native';
+import { Link } from 'expo-router';
+import { Text, View, } from 'react-native';
+
 
 export default function HomeScreen() {
     return <View>
+        <Text>Home Screen</Text>
+        <Link href={"/accounts"}>
+            <Text>Take me to accounts page</Text>
+        </Link>
         
-    </View>
+        
+        <Link href={"/foryou"}>
+            <Text>Take me to for you page</Text>        
+        </Link>
+        
+        
+        <Link href={"/explore"}>
+            <Text>Take me to explore page</Text>
+        </Link>
+
+        </View>;
 }
