6aa69fa49c5f260dafb336a9152be29032d5e71d
deepanshukatariya450@gmail.com <94599793+05Deepanshu@users.noreply.github.com>
2025-06-02 22:06:40 +0530

    updated layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,30 @@
-import { Link, Slot } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
-
-export default function Layout(){
-    return <SafeAreaView>
-        <View style={{height: "90%"}}>
-            <Slot />
-        </View>
-        <View style={{ backgroundColor: "red" }}>
-            <Link href={"/accounts"}>
-                <Text>Take me to accounts page</Text>
-            </Link>
-        
-            <Link href={"/"}>
-                <Text>Take me to for you page</Text>        
-            </Link>
-        
-        
-            <Link href={"/explore"}>
-                <Text>Take me to explore page</Text>
-            </Link>
-            </View>
-            
-        
-    </SafeAreaView>
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'For You',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
-
-// Slot is similar to children in NextJs.
