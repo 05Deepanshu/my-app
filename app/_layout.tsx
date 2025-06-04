4bc081d94b41a9c3fa06d5666086a83126bcad91
deepanshukatariya450@gmail.com <94599793+05Deepanshu@users.noreply.github.com>
2025-06-04 05:54:50 +0530

    refactor: replace Slot with Stack for improved navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,15 @@
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
     return <GestureHandlerRootView>
-        <Slot />
+        <Stack screenOptions={{
+            headerShown: false
+        }}>
+            <Stack.Screen name = "(nobottombar)/accountinfo" options={{headerShown: true,
+            headerTitle: "Account info"}} />
+  
+        
+        </Stack>
     </GestureHandlerRootView>
 }
